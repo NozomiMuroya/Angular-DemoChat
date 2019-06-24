02116416347182e7fc5cf8c311f15f65dc83f9a7
Nojiniy <nonkit3418@yahoo.co.jp>
2019-06-24 19:11:38 +0900

    Angular 途中

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from 'firebase';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(
+    public authService: AuthService,
+    private router:Router
+    ) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.authService.afUser$.pipe(
+      map((afUser: User | null) => {
+        if (afUser) {
+          return true;
+        } else {
+            this.router.navigateByUrl('/login');
+            return false;
+        }
+      })
+    );
+  }
+}
---
+++ src/app/guards/guest.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from 'firebase';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GuestGuard implements CanActivate {
+  constructor(
+    public authService: AuthService,
+    private router:Router
+    ) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.authService.afUser$.pipe(
+      map((afUser: User | null) => {
+        if (!afUser) {
+          return true;
+        } else {
+          this.router.navigateByUrl('/');
+          return false;
+        }
+      })
+    );
+  }
+
+}
